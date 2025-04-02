7043625897ecd71c6fdfd484cc63e106178f6cb0
GianLucca Aguado <71113270+Gianlucca-git@users.noreply.github.com>
2025-04-02 15:59:33 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,25 @@
+from flask import Flask, request
+
+app = Flask(__name__)
+
+
+@app.route('/api', methods=['POST'])
+def upload_file():
+    print('INIT upload_file')
+    if 'file' not in request.files:
+        return "No file part", 400
+    file = request.files['file']
+
+    print('REQUEST ', request)
+    print('request.files ', request.files)
+    print('file', file)
+
+    if file.filename == '':
+        return "No selected file", 400
+    if file:
+        content = file.read().decode('utf-8')
+        return content, 200
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
